2a6922684831812d0aa3f6ed57e033e42fc6b76c
= <=>
2021-08-31 03:09:41 -0500

    add api to return movies based on genera

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ for (let i of document.querySelectorAll(".generasButton")) {
   i.addEventListener("click", () => {
     currentGenera = i.getAttribute("aria-label");
     assignSelectedGeneraColor();
+    addToGeneraContainer(`https://api.themoviedb.org/3/discover/movie?api_key=5f962c263d7b0f3d4790f1a7fec62185&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${currentGenera}&with_watch_monetization_types=flatrate`)
   })
 }
 
-async function getMovies(content) {
-  let getMoviesPromise = await axios.get(`https://api.themoviedb.org/3/movie/${content}?api_key=5f962c263d7b0f3d4790f1a7fec62185&language=en-US&page=1`)
+async function getMovies(APIURL) {
+  let getMoviesPromise = await axios.get(APIURL)
   return getMoviesPromise.data.results
 }
 
 //Collcts the data from getMovies using a content type (popular, top_rated etc...) and calls the helper function to add image posters
-function getPopularAndTopRatedMovies(content) {
-  getMovies(content).then(response => {
+function getPopularAndTopRatedMovies(APIURL, content) {
+  getMovies(APIURL).then(response => {
     for (let i = 0; i < response.length; i++) {
       if (i === 0) {
         createPosters2(response[i].poster_path, true, content)
@@ -46,6 +47,17 @@ function createPosters2(imageURL, isActive, content) {
 
 }
 
+function addToGeneraContainer(APIURL) {
+  getMovies(APIURL).then(response => {
+    for (let i of response) {
+      let newImage = document.createElement("img");
+      newImage.setAttribute("src", `https://image.tmdb.org/t/p/w500${i.poster_path}`)
+      document.querySelector("#moviesContainer").appendChild(newImage)
+    }
+  });
+}
+
+
 function assignSelectedGeneraColor() {
   for (let i of document.querySelectorAll(".generasButton")) {
     if (i.getAttribute("aria-label") == currentGenera) {
@@ -56,5 +68,5 @@ function assignSelectedGeneraColor() {
   }
 }
 
-getPopularAndTopRatedMovies("popular")
-getPopularAndTopRatedMovies("top_rated")
+getPopularAndTopRatedMovies("https://api.themoviedb.org/3/movie/popular?api_key=5f962c263d7b0f3d4790f1a7fec62185&language=en-US&page=1", "popular")
+getPopularAndTopRatedMovies("https://api.themoviedb.org/3/movie/top_rated?api_key=5f962c263d7b0f3d4790f1a7fec62185&language=en-US&page=1", "top_rated")
