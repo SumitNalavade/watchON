be5907101352afb445e0d2f45283ceed687d84c6
SumitNalavade <vs.nalavade2003@gmail.com>
2021-09-13 02:06:51 -0500

    add bootstrap cards to search results

--- static/js/search.js
+++ static/js/search.js
@@ -4,25 +4,41 @@ async function getMovieSearch () {
     return (movieSearchData.data.results);
 }
 
-function createMovieSearchPoster () {
+/*
+ <div class="card">
+                <img src="https://image.tmdb.org/t/p/w500/6Y9fl8tD1xtyUrOHV2MkCYTpzgi.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</h4>
+                </div>
+            </div>
+*/
+
+function createSearchCards () {
     getMovieSearch().then(response => {
         for(let i of response) {
-            let movieSearch = document.createElement("div");
-            movieSearch.classList.add("movieSearch");
-            
-            let movieSearchPoster = document.createElement("img");
-            movieSearchPoster.setAttribute("src", `https://image.tmdb.org/t/p/w500${i.poster_path}`)
-            movieSearchPoster.classList.add("movieSearchPoster");
+            console.log(i.poster_path);
+            let newCard = document.createElement("div");
+            newCard.classList.add("card");
 
-            let movieSearchTitle = document.createElement("h4");
-            movieSearchTitle.innerText = i.title;
+            let newPoster = document.createElement("img");
+            newPoster.setAttribute("src", `https://image.tmdb.org/t/p/w500${i.poster_path}`)
+            newPoster.classList.add("card-img-top");
+            newCard.appendChild(newPoster);
 
-            movieSearch.appendChild(movieSearchPoster);
-            movieSearch.append(movieSearchTitle);
+            let newCardBody = document.createElement("div");
+            newCardBody.classList.add("card-body");
+            newCard.appendChild(newCardBody);
 
-            movieSearchContainer.appendChild(movieSearch);
+            let newCardText = document.createElement("h4");
+            newCardText.classList.add("card-text");
+            newCardText.innerText = i.title;
+            newCardBody.appendChild(newCardText);
+
+            document.querySelector("#movieSearchContainer").appendChild(newCard);
+
         }
     })
 }
 
-createMovieSearchPoster();
+createSearchCards();
