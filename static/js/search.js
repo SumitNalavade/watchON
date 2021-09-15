97fe5dc5cf4a7ad0e9b9e4df7fefb7725de7ccf1
SumitNalavade <vs.nalavade2003@gmail.com>
2021-09-15 02:36:12 -0500

    .

--- static/js/search.js
+++ static/js/search.js
@@ -1,26 +1,26 @@
-async function getMovieSearch () {
+async function getMovieSearch() {
     let movieSearchData = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=5f962c263d7b0f3d4790f1a7fec62185&language=en-US&query=${searchQuery}&page=1&include_adult=false`)
 
     return (movieSearchData.data.results);
 }
 
-function createSearchImage (imageURL) {
+function createSearchImage(imageURL) {
     let newPoster = document.createElement("img");
     newPoster.setAttribute("src", `https://image.tmdb.org/t/p/w500${imageURL}`)
     newPoster.classList.add("card-img-top");
-    
+
     return newPoster
 }
 
-function createSearchTitle (title) {
+function createSearchTitle(title) {
     let newCardText = document.createElement("h4");
     newCardText.classList.add("card-text", "modalTitle");
     newCardText.innerText = title;
 
     return newCardText
-} 
+}
 
-function createSearchDescription (description) {
+function createSearchDescription(description) {
     let newDescriptionText = document.createElement("p");
     newDescriptionText.classList.add("modalDescription");
     newDescriptionText.innerText = description;
@@ -28,10 +28,9 @@ function createSearchDescription (description) {
     return newDescriptionText
 }
 
-
-function createSearchCards () {
+function createSearchCards() {
     getMovieSearch().then(response => {
-        for(let i of response) {
+        for (let i of response) {
             let newCard = document.createElement("div");
             newCard.classList.add("card");
 
